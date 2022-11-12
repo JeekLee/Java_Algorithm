1dd3227a4d1c80d3a952d8839f187e8bec7b1e93
JeekLee <j3kkun@gmail.com>
2022-11-12 20:27:54 +0900

    221112 Algorithm, Class, Inheritance

---
+++ basicGrammar/object_class/Car.java
@@ -0,0 +1,45 @@
+package basicGrammar.object_class;
+
+public class Car {
+    // field
+    String company;
+    String model;
+    String color;
+    int maxSpeed;
+    int speed;
+
+    // constructor, Overloading
+    Car() {
+        this("현대자동차", "그랜저", "회색", 250);
+    }
+    Car(String company) {
+        this(company, "그랜저", "회색", 250);
+    }
+    Car(String company, String model) {
+        this(company, model, "회색", 250);
+    }
+    Car(String company, String model, String color) {
+        this(company, model, color, 250);
+    }
+    Car(String company, String model, String color, int maxSpeed) {
+        this.company = company;
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+
+    // methods
+    void powerOn() {
+        System.out.println("시동을 켭니다.");
+        System.out.println(this.company + "/" + this.model + "/"  + this.color + "/"  + this.maxSpeed);
+    }
+    void powerOff() {
+        System.out.println("시동을 끕니다.");
+        System.out.println(this.company + "/" + this.model + "/"  + this.color + "/"  + this.maxSpeed);
+    }
+    int calcTime(int distance){
+        return distance/this.maxSpeed;
+    }
+
+
+}
---
+++ programmers/food_fight/FoodFight.java
@@ -0,0 +1,42 @@
+package programmers.food_fight;
+
+import java.util.Scanner;
+
+public class FoodFight {
+    public static String addnum(String answer, int count, int foodnum){
+        for (int i = 0; i < count; i++) {
+            answer = foodnum + answer + foodnum;
+        }
+        return answer;
+    }
+    public static String solution(int[] food){
+        String answer = "0";
+        for (int i = food.length-1 ; i > 0 ; i--){
+            int count = food[i]/2;
+            answer = addnum(answer, count, i);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = 0;
+        while( m < 2 || m > 9){
+            System.out.print("food의 길이를 입력하시오. (2~9)> ");
+            m = scanner.nextInt();
+            scanner.nextLine();
+        }
+        int[] food = new int[m];
+
+        for (int i = 0; i< m; i++){
+            int k = 0;
+            while ( k < 1 || k > 1000){
+                System.out.printf("%d번 째 음식의 수를 입력하시오. (1~1000)> ", i+1);
+                k = scanner.nextInt();
+                scanner.nextLine();
+                food[i] = k;
+            }
+        }
+
+        System.out.println(solution(food));
+    }
+}
